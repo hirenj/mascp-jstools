ba81eba470e036af74f83a040bcabe17b6a44ba6
Hiren Joshi <hirenj@gmail.com>
2010-03-29 18:08:52 -0700

    Upgrading the api for the Promex reader

--- js/lib/PromexReader.js
+++ js/lib/PromexReader.js
@@ -16,15 +16,16 @@ MASCP.PromexReader = MASCP.buildService(function(data) {
                         return this;
                     });
 
+MASCP.PromexReader.SERVICE_URL = 'http://131.130.57.242/json/';
+
 MASCP.PromexReader.prototype.requestData = function()
 {
     var agi = (this.agi+"").replace(/\..*$/,'');
     
     return {
-        type: "POST",
-        dataType: "xml",
-        data: { 'action'    : 11,
-                'txt_query' : agi,
+        type: "GET",
+        dataType: "json",
+        data: { 'ac'        : agi,
                 'service'   : 'promex' 
         }
     };
@@ -37,10 +38,9 @@ MASCP.PromexReader.prototype.requestData = function()
 // We need this line for the JsDoc to pick up this class
 MASCP.PromexReader.Result = MASCP.PromexReader.Result;
 
-/** Retrieve the URLS for the spectra from the Promex service
+/* Retrieve the URLS for the spectra from the Promex service
  *  @returns    Array of urls for the spectra for this entry
  *  @type [String]
- */
 MASCP.PromexReader.Result.prototype.getSpectraURLs = function()
 {
     var bleh = this._raw_data;
@@ -60,7 +60,7 @@ MASCP.PromexReader.Result.prototype.getSpectraURLs = function()
     }
     return urls;
 };
-
+*/
 
 /** Retrieve the peptides for this particular entry from the Promex service
  *  @returns Array of peptide strings
@@ -70,24 +70,13 @@ MASCP.PromexReader.Result.prototype.getPeptides = function()
 {
     var content = null;
     
-    if (this._raw_data.getElementById) {
-        content = this._raw_data.getElementById('contentArea');
-    } else {
-        content = this._raw_data.selectSingleNode("//*[@id = 'contentArea']");
-    }
-    
-    if (! content ) {
+    if (! this._raw_data ) {
         return [];
-    }
-    
-    var tds = jQuery(content.getElementsByTagName('td'));
+    }    
     var peptides = [];
-    var result = this;
-    tds.each(function(i) {
-        if (jQuery(this).text() == 'peptide sequence') {
-            peptides.push(result._cleanSequence(jQuery(tds[i+1]).text()));            
-        }
-    });
+    for (var i = 0; i < this._raw_data.peptides.length; i++ ) {
+        peptides.push(this._cleanSequence(this._raw_data.peptides[i].sequence));
+    }
     return peptides;
 };
 
@@ -101,32 +90,41 @@ MASCP.PromexReader.prototype.setupSequenceRenderer = function(sequenceRenderer)
     var reader = this;
     MASCP.registerGroup('promex_experimental', {'fullname' : 'ProMex spectra data', 'hide_member_controllers' : true, 'hide_group_controller' : true, 'color' : '#ff9900' });
 
+    var overlay_name = 'promex_controller';
+
+    var css_block = '.active .overlay { background: #ff9900; } .active a { color: #000000; text-decoration: none !important; }  :indeterminate { background: #ff0000; } .tracks .active { background: #0000ff; } .inactive a { text-decoration: none; } .inactive { display: none; }';
+    
+    MASCP.registerLayer(overlay_name,{ 'fullname' : 'Promex results', 'color' : '#ff9900', 'css' : css_block });
+
     this.bind('resultReceived', function() {
-        var spectra = this.result.getSpectraURLs();
         // var css_block = '.active { background: #ff9900; color: #ffffff;} :indeterminate { background: #ff0000; } .active a:hover { background: transparent !important; } .inactive { }';
-        var css_block = '.active .overlay { background: #ff9900; } .active a { color: #000000; text-decoration: none !important; }  :indeterminate { background: #ff0000; } .tracks .active { background: #0000ff; } .inactive a { text-decoration: none; } .inactive { display: none; }';
-    	jQuery(this.result.getPeptides()).each(function(i) {
-            
-        	MASCP.registerLayer('promex_experimental_spectrum_'+i, { 'fullname': 'ProMex spectrum', 'group' : 'promex_experimental', 'color' : '#ff9900', 'css' : css_block });
-    	    var a_spectra = spectra[i];
-    	    var peptide = this;
-    	    var peptide_bits = sequenceRenderer.getAminoAcidsByPeptide(peptide);
-    	    var layer_name = 'promex_experimental_spectrum_'+i;
-    	    peptide_bits[0].addToLayerWithLink(layer_name,a_spectra,peptide_bits.length);
-    	    jQuery(MASCP.getLayer('promex_experimental_spectrum_'+i)).bind('click',function() {
-    	        window.open(a_spectra);
-    	    });
-    	});
-    	jQuery(sequenceRenderer).trigger('resultsRendered',[reader]);        
+        var peps = this.result.getPeptides();
+        for(var i = 0; i < peps.length; i++) {
+            MASCP.registerLayer('promex_experimental_spectrum_'+i, { 'fullname': 'ProMex spectrum', 'group' : 'promex_experimental', 'color' : '#ff9900', 'css' : css_block });
+            var peptide = peps[i];
+            var peptide_bits = sequenceRenderer.getAminoAcidsByPeptide(peptide);
+            var layer_name = 'promex_experimental_spectrum_'+i;
+            peptide_bits[0].addBoxOverlay(layer_name,1,peptide_bits.length);
+            peptide_bits[0].addBoxOverlay(overlay_name,1,peptide_bits.length);
+            // jQuery(MASCP.getLayer('promex_experimental_spectrum_'+i)).bind('click',function() {
+            //     window.open(a_spectra);
+            // });
+        }
+        jQuery(sequenceRenderer).trigger('resultsRendered',[reader]);        
+
+        if (sequenceRenderer.createGroupController) {
+            sequenceRenderer.createGroupController('promex_controller','promex_experimental');
+        }
+
+
     })
     return this;
 };
 
 MASCP.PromexReader.Result.prototype.render = function()
 {
-    if (this.getSpectraURLs().length > 0) {
-        var params = jQuery.param(this.reader.requestData()['data']);
-        var a_container = jQuery('<div>MS/MS spectra <input class="group_toggle" type="checkbox"/><a style="display: block; float: right;" href="http://www.promexdb.org/cgi-bin/peplib.pl?'+params+'">ProMEX</a></div>');
+    if (this.getPeptides().length > 0) {
+        var a_container = jQuery('<div>MS/MS spectra <input class="group_toggle" type="checkbox"/><a style="display: block; float: right;" href="http://www.promexdb.org/">ProMEX</a></div>');
         jQuery(this.reader.renderers).each(function(i){
             this.createGroupCheckbox('promex_experimental',jQuery('input.group_toggle',a_container));
         });
