cecfe904a03481f0fefb94d7b1814d2cc0e35870
Hiren Joshi <hirenj@gmail.com>
2010-05-11 15:05:20 -0700

    Add in AtPeptide service
    Former-commit-id: 8ce29c4c427a0d9585ad69f736fffa60650242e3

---
+++ js/lib/AtPeptideReader.js
@@ -0,0 +1,108 @@
+/** @fileOverview   Classes for reading data from the AtPeptide database
+ */
+if ( typeof MASCP == 'undefined' || typeof MASCP.Service == 'undefined' ) {
+    throw "MASCP.Service is not defined, required class";
+}
+
+/** Default class constructor
+ *  @class      Service class that will retrieve data from AtPeptide for a given AGI.
+ *  @param      {String} agi            Agi to look up
+ *  @param      {String} endpointURL    Endpoint URL for this service
+ *  @extends    MASCP.Service
+ */
+MASCP.AtPeptideReader = MASCP.buildService(function(data) {
+                        this._raw_data = data;                        
+                        return this;
+                    });
+
+MASCP.AtPeptideReader.SERVICE_URL = 'http://172.16.128.128/static/atpeptide.pl';
+
+MASCP.AtPeptideReader.prototype.requestData = function()
+{
+    var agi = this.agi;
+    
+    return {
+        type: "GET",
+        dataType: "json",
+        data: { 'agi'       : agi,
+                'service'   : 'atpeptide' 
+        }
+    };
+};
+
+/**
+ *  @class   Container class for results from the AtPeptide service
+ *  @extends MASCP.Service.Result
+ */
+// We need this line for the JsDoc to pick up this class
+MASCP.AtPeptideReader.Result = MASCP.AtPeptideReader.Result;
+
+/** Retrieve the peptides for this particular entry from the AtPeptide service
+ *  @returns Array of peptide strings
+ *  @type [String]
+ */
+MASCP.AtPeptideReader.Result.prototype.getPeptides = function()
+{
+    var content = null;
+    
+    if (! this._raw_data || ! this._raw_data.peptides ) {
+        return [];
+    }    
+    var peptides = [];
+    for (var i = 0; i < this._raw_data.peptides.length; i++ ) {
+        peptides.push(this._cleanSequence(this._raw_data.peptides[i].sequence));
+    }
+    return peptides;
+};
+
+MASCP.AtPeptideReader.Result.prototype._cleanSequence = function(sequence)
+{
+    return sequence.replace(/[^A-Z]/g,'');
+};
+
+MASCP.AtPeptideReader.prototype.setupSequenceRenderer = function(sequenceRenderer)
+{
+    var reader = this;
+    MASCP.registerGroup('atpeptide_experimental', {'fullname' : 'AtPeptide MS/MS', 'hide_member_controllers' : true, 'hide_group_controller' : true, 'color' : '#ff5533' });
+
+    var overlay_name = 'atpeptide_controller';
+
+    var css_block = '.active .overlay { background: #ff5533; } .active a { color: #000000; text-decoration: none !important; }  :indeterminate { background: #ff0000; } .tracks .active { background: #0000ff; } .inactive a { text-decoration: none; } .inactive { display: none; }';
+    
+    MASCP.registerLayer(overlay_name,{ 'fullname' : 'AtPeptide MS/MS', 'color' : '#ff5533', 'css' : css_block });
+
+
+    this.bind('resultReceived', function() {
+                
+        var peps = this.result.getPeptides();
+        for(var i = 0; i < peps.length; i++) {
+            MASCP.registerLayer('atpeptide_peptide_'+i, { 'fullname': 'Peptide', 'group' : 'atpeptide_experimental', 'color' : '#ff5533', 'css' : css_block });
+            var peptide = peps[i];
+            var peptide_bits = sequenceRenderer.getAminoAcidsByPeptide(peptide);
+            var layer_name = 'atpeptide_peptide_'+i;
+            peptide_bits[0].addBoxOverlay(layer_name,1,peptide_bits.length);
+            peptide_bits[0].addBoxOverlay(overlay_name,1,peptide_bits.length);
+        }
+        jQuery(sequenceRenderer).trigger('resultsRendered',[reader]);        
+
+        if (sequenceRenderer.createGroupController) {
+            sequenceRenderer.createGroupController('atpeptide_controller','atpeptide_experimental');
+        }
+
+
+    })
+    return this;
+};
+
+MASCP.AtPeptideReader.Result.prototype.render = function()
+{
+    if (this.getPeptides().length > 0) {
+        var a_container = jQuery('<div>MS/MS spectra <input class="group_toggle" type="checkbox"/>AtPeptide</div>');
+        jQuery(this.reader.renderers).each(function(i){
+            this.createGroupCheckbox('atpeptide_experimental',jQuery('input.group_toggle',a_container));
+        });
+        return a_container;
+    } else {
+        return null;
+    }
+};
