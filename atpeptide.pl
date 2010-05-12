9dffe584863e5ad93e09bdfd575c72702c36ba87
Hiren Joshi <hirenj@gmail.com>
2010-05-11 18:16:10 -0700

    Add in drop shadows properly
    Former-commit-id: 48bd0e891a9e16a79f3fa1706227813b3c56cf8d

--- atpeptide.pl
+++ atpeptide.pl
@@ -4,7 +4,7 @@ use CGI;
 
 my $cgi = new CGI();
 
-my $agi = uc $cgi->param('agi');
+my $agi = uc( $cgi->param('agi') || '');
 
 if ( $agi =~ /^AT[0-9A-Z]G\d+\.\d/) {
     print $cgi->header(-Content_type => 'application/json', -Access_Control_Allow_Origin => '*', -Access_Control_Allow_Methods => '*', -Access_Control_Max_Age => '1728000', -Access_Control_Allow_Headers => 'x-requested-with');
@@ -14,4 +14,6 @@ if ( $agi =~ /^AT[0-9A-Z]G\d+\.\d/) {
             print $_;
         }
     }
+} else {
+    print $cgi->header(-Content_type => 'application/json', -Access_Control_Allow_Origin => '*', -Access_Control_Allow_Methods => '*', -Access_Control_Max_Age => '1728000', -Access_Control_Allow_Headers => 'x-requested-with');    
 }
