83de4589b45e713823fa15eb41eeb9d6d8f803af
Hiren Joshi <hirenj@gmail.com>
2010-08-19 16:55:55 -0700

    Add in support for retrieving data from PPDB

--- proxy.pl
+++ proxy.pl
@@ -37,12 +37,14 @@ my $URLS = {
   'atproteome' => 'http://fgcz-atproteome.unizh.ch/index.php',
   'atproteome-json' => 'http://fgcz-atproteome.unizh.ch/mascpv2.php',
   'tair'    => 'http://www.arabidopsis.org/servlets/TairObject',
+  'ppdb'    => 'http://ppdb.tc.cornell.edu/das/arabidopsis/features/',
 };
 
 my $tidy = {
     'promex' => true,
     'atproteome' => true,
     'tair' => true,
+    'ppdb' => 'xml'
 };
 
 my $service = $cgi->param('service');
@@ -58,6 +60,8 @@ print STDERR "Determined service is ${service}";
 
 my $tmpdir = File::Spec->tmpdir();
 my $cached = File::Spec->catfile($tmpdir,"masc-".$hash->hexdigest);
+
+print STDERR "Looking for cached file ${cached}";
 
 if ( -e $cached )
 {
@@ -77,21 +81,32 @@ if ( -e $cached )
 
 my $request;
 if ($method eq 'GET') {
-    $request = HTTP::Request->new('POST', "${url}?${data}");
+    $request = HTTP::Request->new('GET', "${url}?${data}");
 } else {
     $request = HTTP::Request->new('POST', $url,new HTTP::Headers( Content_Type => 'application/x-www-form-urlencoded' ),$data);
 }
 
-if ($tidy->{$service}) {
+
+if ($tidy->{$service} eq 'xml') {
     print CGI->header(-Content_type => 'application/xml',-Access_Control_Allow_Origin => '*', -Access_Control_Allow_Methods => '*', -Access_Control_Max_Age => '1728000', -Access_Control_Allow_Headers => 'x-requested-with');
-    my $tidy = HTML::Tidy->new( { output_xml => 1, add_xml_decl => 1 });
+    my $start_content = LWP::UserAgent->new->request($request)->content;
+    my $cleaned = $start_content;
+    print $cleaned;
+    $cleaned =~ s/nbsp//mgi;
+    $cleaned =~ s/\&/&amp;/mgi;
+    open $outfile, ">".$cached;
+    print $outfile $cleaned;
+    close $outfile;    
+} elsif ($tidy->{$service}) {
+    print CGI->header(-Content_type => 'application/xml',-Access_Control_Allow_Origin => '*', -Access_Control_Allow_Methods => '*', -Access_Control_Max_Age => '1728000', -Access_Control_Allow_Headers => 'x-requested-with');
+    my $tidyer = HTML::Tidy->new( { output_xml => 1, add_xml_decl => 1 });
     my $start_content = LWP::UserAgent->new->request($request)->content;
     my $repl_doctype = qq|<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">|;
     $start_content =~ s/<\!DOCTYPE[^>]+>/TEMP_DTYPE/mi;
     $start_content =~ s/<\!DOCTYPE[^>]+>//mi;
     $start_content =~ s/TEMP_DTYPE/$repl_doctype/mi;
     $start_content =~ s/nbsp//mgi;
-    my $cleaned = $tidy->clean( $start_content );
+    my $cleaned = $tidyer->clean( $start_content );
     print $cleaned;
 #    $cleaned = 'Foo';
     $cleaned =~ s/nbsp//mgi;
