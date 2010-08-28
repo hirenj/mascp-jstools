4a827a7373c62fb7c337ddaa13077e18f9cf4cd0
Hiren Joshi <hirenj@gmail.com>
2010-08-27 17:35:53 -0700

    Add in Rippdb

---
+++ rippdb.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+use CGI;
+
+my $cgi = new CGI();
+
+my $agi = uc( $cgi->param('agi') || '');
+
+if ( $agi =~ /^AT[0-9A-Z]G\d+\.\d+/) {
+    print $cgi->header(-Content_type => 'application/json', -Access_Control_Allow_Origin => '*', -Access_Control_Allow_Methods => '*', -Access_Control_Max_Age => '1728000', -Access_Control_Allow_Headers => 'x-requested-with');
+    open RIPPDB, 'rippdb.txt';
+    while (<RIPPDB>) {    
+        if (/$agi/) {            
+            warn $_;
+            warn $agi;
+            print $_;
+        }
+    }
+} else {
+    print $cgi->header(-Content_type => 'application/json', -Access_Control_Allow_Origin => '*', -Access_Control_Allow_Methods => '*', -Access_Control_Max_Age => '1728000', -Access_Control_Allow_Headers => 'x-requested-with');    
+}
