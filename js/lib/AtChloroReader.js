e9a66c46b3b0483dc71f98fddcea3b4dd37297e7
Hiren Joshi <hirenj@gmail.com>
2011-02-15 12:27:49 -0800

    Add in AT_CHLORO resource
    Former-commit-id: 7b7f950381c98fa8cf2b9afa91aaa954c83b79f2

---
+++ js/lib/AtChloroReader.js
@@ -0,0 +1,111 @@
+/** @fileOverview   Classes for reading data from the AtChloro database
+ */
+if ( typeof MASCP == 'undefined' || typeof MASCP.Service == 'undefined' ) {
+    throw "MASCP.Service is not defined, required class";
+}
+
+/** Default class constructor
+ *  @class      Service class that will retrieve data from AtChloro for a given AGI.
+ *  @param      {String} agi            Agi to look up
+ *  @param      {String} endpointURL    Endpoint URL for this service
+ *  @extends    MASCP.Service
+ */
+MASCP.AtChloroReader = MASCP.buildService(function(data) {
+                        console.log(data);
+                        this._raw_data = data;                        
+                        return this;
+                    });
+
+MASCP.AtChloroReader.SERVICE_URL = 'http://prabi2.inrialpes.fr/test_at_chloro/annotation/';
+
+MASCP.AtChloroReader.prototype.requestData = function()
+{
+    var agi = this.agi;
+    
+    return {
+        type: "GET",
+        url: this._endpointURL + agi.toUpperCase(),
+        dataType: "json",
+        data: { 'agi'       : agi.toUpperCase(),
+                'service'   : 'atchloro' 
+        }
+    };
+};
+
+
+/**
+ *  @class   Container class for results from the AtChloro service
+ *  @extends MASCP.Service.Result
+ */
+// We need this line for the JsDoc to pick up this class
+MASCP.AtChloroReader.Result = MASCP.AtChloroReader.Result;
+
+/** Retrieve the peptides for this particular entry from the AtChloro service
+ *  @returns Array of peptide strings
+ *  @type [String]
+ */
+MASCP.AtChloroReader.Result.prototype.getPeptides = function()
+{
+    var content = null;
+
+    if (this._peptides) {
+        return this._peptides;
+    }
+
+    this._long_name_map = {};
+    
+    if (! this._raw_data || ! this._raw_data.peptides ) {
+        return [];
+    }
+
+        
+    var peptides = [];
+    
+    for (var i = 0; i < this._raw_data.peptides.length; i++ ) {
+        var a_peptide = this._raw_data.peptides[i];
+        var the_pep = { 'sequence' : this._cleanSequence(a_peptide.sequence) };
+        peptides.push(the_pep);
+    }
+    this._peptides = peptides;
+    return peptides;
+};
+
+MASCP.AtChloroReader.Result.prototype._cleanSequence = function(sequence)
+{
+    return sequence.replace(/[^A-Z]/g,'');
+};
+
+MASCP.AtChloroReader.prototype.setupSequenceRenderer = function(sequenceRenderer)
+{
+    var reader = this;
+
+    var css_block = '.active .overlay { background: #55ff33; } .active a { color: #000000; text-decoration: none !important; }  :indeterminate { background: #ff0000; } .tracks .active { background: #0000ff; } .inactive a { text-decoration: none; } .inactive { display: none; }';
+    
+
+    this.bind('resultReceived', function() {
+        var peps = this.result.getPeptides();
+        if (peps.length > 0) {
+            MASCP.registerLayer('atchloro_experimental',{ 'fullname' : 'AT_CHLORO MS/MS', 'color' : '#55ff33', 'css' : css_block });
+        }
+        for(var i = 0; i < peps.length; i++) {
+            var peptide = peps[i].sequence;
+            var peptide_bits = sequenceRenderer.getAminoAcidsByPeptide(peptide);
+            peptide_bits.addToLayer('atchloro_experimental');
+        }
+        jQuery(sequenceRenderer).trigger('resultsRendered',[reader]);
+    })
+    return this;
+};
+
+MASCP.AtChloroReader.Result.prototype.render = function()
+{
+    if (this.getPeptides().length > 0) {
+        var a_container = jQuery('<div>MS/MS spectra <input class="group_toggle" type="checkbox"/>AtChloro</div>');
+        jQuery(this.reader.renderers).each(function(i){
+            this.createGroupCheckbox('atchloro_experimental',jQuery('input.group_toggle',a_container));
+        });
+        return a_container;
+    } else {
+        return null;
+    }
+};
