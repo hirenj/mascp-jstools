ebb6cfa5c98ddc04a58c2d58dcd70809b66a3e89
Hiren Joshi <hirenj@gmail.com>
2011-04-12 18:45:05 -0700

    Add in cli interface to the services

--- node.js
+++
@@ -1,7 +0,0 @@
-MASCP = require('./dist/js/maschup.services.min.js');
-suba = new MASCP.TairReader();
-suba.agi = "at1g22710.1";
-setTimeout(function() {
-//    suba.bind('resultReceived',);
-    suba.retrieve('at1g22710.1',function() { console.log(this.result.getSequence()); });
-},1000);
