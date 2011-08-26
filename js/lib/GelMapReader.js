111d21e87e72b28f693e707a0f7fec9e63cf183c
Hiren Joshi <hirenj@gmail.com>
2011-08-26 13:25:05 -0700

    Class file for reading data from the GelMap resource

---
+++ js/lib/GelMapReader.js
@@ -0,0 +1,124 @@
+/** @fileOverview   Classes for reading data from the AtPeptide database
+ */
+if ( typeof MASCP === 'undefined' || typeof MASCP.Service === 'undefined' ) {
+    throw "MASCP.Service is not defined, required class";
+}
+
+/** Default class constructor
+ *  @class      Service class that will retrieve data from AtPeptide for a given AGI.
+ *  @param      {String} agi            Agi to look up
+ *  @param      {String} endpointURL    Endpoint URL for this service
+ *  @extends    MASCP.Service
+ */
+MASCP.GelMapReader = MASCP.buildService(function(data) {
+                        this._raw_data = data;
+                        if (! data) {
+                            return this;
+                        }
+                        var maps = [];
+                        for (var mapk in data) {
+                            var map = data[mapk];
+                            map.id = map.mapID;
+                            map.title = map.mapTitle;
+                            map.peptides = map.sequence;
+                            map.sequence = "";
+                            maps.push(map);
+                        }
+                        this.maps = maps;
+                        return this;
+                    });
+
+MASCP.GelMapReader.SERVICE_URL = ' http://gelmap.de/gator.php';
+
+MASCP.GelMapReader.prototype.requestData = function()
+{
+    var agi = this.agi.toUpperCase();
+    
+    return {
+        type: "GET",
+        dataType: "json",
+        data: { 'agi'       : agi,
+                'service'   : 'gelmap' 
+        }
+    };
+};
+
+/**
+ *  @class   Container class for results from the service
+ *  @extends MASCP.Service.Result
+ */
+// We need this line for the JsDoc to pick up this class
+MASCP.GelMapReader.Result = MASCP.GelMapReader.Result;
+
+/** Retrieve the peptides for this particular entry from the service
+ *  @returns Array of peptide strings
+ *  @type [String]
+ */
+MASCP.GelMapReader.Result.prototype.getPeptides = function()
+{
+    var content = null;
+
+    if (this._peptides) {
+        return this._peptides;
+    }
+    
+    
+    this._peptides = peptides;
+    
+    return peptides;
+};
+
+MASCP.GelMapReader.Result.prototype._cleanSequence = function(sequence)
+{
+    return sequence.replace(/[^A-Z]/g,'');
+};
+
+MASCP.GelMapReader.prototype.setupSequenceRenderer = function(sequenceRenderer)
+{
+    var reader = this;
+    MASCP.registerGroup('gelmap_experimental', {'fullname' : 'GelMap', 'hide_member_controllers' : true, 'hide_group_controller' : true, 'color' : '#aaaaff' });
+
+    var controller_name = 'gelmap_controller';
+
+    var css_block = '.active .overlay { background: #ff5533; } .active a { color: #000000; text-decoration: none !important; }  :indeterminate { background: #ff0000; } .tracks .active { background: #0000ff; } .inactive a { text-decoration: none; } .inactive { display: none; }';
+    
+    MASCP.registerLayer(controller_name,{ 'fullname' : 'GelMap', 'color' : '#aaaaff', 'css' : css_block });
+
+    if (sequenceRenderer.createGroupController) {
+        sequenceRenderer.createGroupController('gelmap_controller','gelmap_experimental');
+    }
+
+    var sort_unique = function(arr) {
+        arr = arr.sort(function (a, b) { return a*1 - b*1; });
+        var ret = [arr[0]];
+        for (var i = 1; i < arr.length; i++) { // start loop at 1 as element 0 can never be a duplicate
+            if (arr[i-1] !== arr[i]) {
+                ret.push(arr[i]);
+            }
+        }
+        return ret;
+    };
+
+    this.bind('resultReceived', function() {
+        for (var maps = this.result.maps, j = maps.length - 1; j >= 0; j--) {
+            var a_map = maps[j];
+            MASCP.registerLayer('gelmap_map_'+a_map.id, { 'fullname': a_map.title, 'group' : 'gelmap_experimental', 'color' : '#aaaaff', 'css' : css_block });
+            MASCP.getLayer('gelmap_map_'+a_map.id).href = a_map.url;
+            var peps = sort_unique(maps[j].peptides);
+
+            for(var i = peps.length - 1; i >= 0; i--) {
+                var peptide = peps[i];
+                var peptide_bits = sequenceRenderer.getAminoAcidsByPeptide(peptide);
+                var layer_name = 'gelmap_map_'+a_map.id;
+                peptide_bits.addToLayer(layer_name);
+                peptide_bits.addToLayer(controller_name);
+            }
+        }
+        jQuery(sequenceRenderer).trigger('resultsRendered',[reader]);
+    })
+    return this;
+};
+
+MASCP.GelMapReader.Result.prototype.render = function()
+{
+};
