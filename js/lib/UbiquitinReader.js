30fa2648cfdeac7510634886f538678e685a2a63
Hiren Joshi <hirenj@gmail.com>
2012-02-15 14:07:57 +0100

    Write the ubiquitin reader class
    Former-commit-id: 1c2a54ec610fed4d79a6e2b34d41cf9a3904f33b

---
+++ js/lib/UbiquitinReader.js
@@ -0,0 +1,132 @@
+/** @fileOverview   Classes for reading data from the Ubiquitin data
+ */
+if ( typeof MASCP == 'undefined' || typeof MASCP.Service == 'undefined' ) {
+    throw "MASCP.Service is not defined, required class";
+}
+
+/** Default class constructor
+ *  @class      Service class that will retrieve data from the Ubiquitin data for a given AGI.
+ *  @param      {String} agi            Agi to look up
+ *  @param      {String} endpointURL    Endpoint URL for this service
+ *  @extends    MASCP.Service
+ */
+MASCP.UbiquitinReader = MASCP.buildService(function(data) {
+                        this._raw_data = data ? data.data : null;
+                        return this;
+                    });
+
+MASCP.UbiquitinReader.SERVICE_URL = '?';
+
+MASCP.UbiquitinReader.prototype.requestData = function()
+{
+    var agi = this.agi;
+    
+    return {
+        type: "GET",
+        dataType: "json",
+        data: { 'agi'       : agi,
+                'service'   : 'ubiquitin' 
+        }
+    };
+};
+
+/**
+ *  @class   Container class for results from the Ubiquitin service
+ *  @extends MASCP.Service.Result
+ */
+// We need this line for the JsDoc to pick up this class
+MASCP.UbiquitinReader.Result = MASCP.UbiquitinReader.Result;
+
+/** Retrieve the peptides for this particular entry from the Ubiquitin service
+ *  @returns Array of peptide strings
+ *  @type [String]
+ */
+MASCP.UbiquitinReader.Result.prototype.getPeptides = function()
+{
+    var content = null;
+    if (! this._raw_data || ! this._raw_data.peptides ) {
+        return [];
+    }
+
+    return this._raw_data.peptides;
+};
+
+MASCP.UbiquitinReader.Result.prototype._cleanSequence = function(sequence)
+{
+    return sequence.replace(/[^A-Z]/g,'');
+};
+
+MASCP.UbiquitinReader.prototype.setupSequenceRenderer = function(sequenceRenderer)
+{
+    var reader = this;
+
+    var css_block = '.active .overlay { background: #666666; } .active a { color: #000000; text-decoration: none !important; }  :indeterminate { background: #ff0000; } .tracks .active { background: #0000ff; } .inactive a { text-decoration: none; } .inactive { display: none; }';
+    
+    this.bind('resultReceived', function() {
+        var peps = this.result.getPeptides();
+
+        var overlay_name = 'ubiquitin_experimental';
+        var group_name = 'ubiquitin_peptides';
+        var icons = [];
+        
+        if (peps.length > 0) {
+            MASCP.registerLayer(overlay_name,{ 'fullname' : 'Ubiquitin (mod)', 'color' : '#666666', 'css' : css_block });
+
+            MASCP.registerGroup(group_name, {'fullname' : 'Ubiquitin', 'hide_member_controllers' : true, 'hide_group_controller' : true, 'color' : '#666666' });
+            if (sequenceRenderer.createGroupController) {
+                sequenceRenderer.createGroupController(overlay_name,group_name);
+            }
+            
+            jQuery(MASCP.getGroup(group_name)).bind('visibilityChange',function(e,rend,vis) {
+                if (rend != sequenceRenderer) {
+                    return;
+                }
+                icons.forEach(function(el) {
+                    el.style.display = vis ? 'none' : 'inline';
+                });
+            });
+            
+            
+        }
+
+        for (var i = 0; i < peps.length; i++) {
+            var layer_name = 'ubiquitin_peptide_'+i;
+            MASCP.registerLayer(layer_name, { 'fullname': 'Peptide '+i, 'group' : group_name, 'color' : '#666666', 'css' : css_block });
+            var peptide = peps[i].sequence;
+            var peptide_bits = sequenceRenderer.getAminoAcidsByPeptide(peptide);
+            if (peptide_bits.length === 0){
+                continue;
+            }
+            peptide_bits.addToLayer(layer_name);
+            icons.push(peptide_bits.addToLayer(layer_name));
+
+            for (var k = 0; k < peps[i].positions.length; k++ ) {
+                icons = icons.concat(peptide_bits[peps[i].positions[k] - 1].addToLayer(overlay_name));
+                peptide_bits[peps[i].positions[k] - 1].addToLayer(layer_name);
+            }
+        }
+        jQuery(sequenceRenderer).trigger('resultsRendered',[reader]);
+    });
+    return this;
+};
+/** Retrieve an array of positions that ubiquitin has been experimentally verified to occur upon
+ *  @returns {Array}    Ubiquitin positions upon the full protein
+ */
+MASCP.UbiquitinReader.Result.prototype.getAllExperimentalPositions = function()
+{
+    var peps = this.getPeptides();
+    var results = [];
+    var seen = {};
+    peps.forEach(function(pep) {
+        pep.positions.forEach(function(pos) {
+            if ( ! seen[pos] ) {
+                results.push(pos);
+                seen[pos] = true;
+            }
+        });
+    });
+    return results;
+}
+MASCP.UbiquitinReader.Result.prototype.render = function()
+{
+};
