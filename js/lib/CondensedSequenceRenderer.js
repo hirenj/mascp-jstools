8a0b4b2f54a51895ef9a8edacba603d5ac6e78f6
Hiren Joshi <hirenj@gmail.com>
2012-04-10 14:09:25 +0200

    Fix for the individual amino acid style rendering
    They were not being rendered correctly, as they
    were shifted way up. This aligns them properly.
    
    
    Former-commit-id: e437f22c855bd0f07827f85eea6c0fe3a95ce744

--- js/lib/CondensedSequenceRenderer.js
+++ js/lib/CondensedSequenceRenderer.js
@@ -243,7 +243,8 @@ MASCP.CondensedSequenceRenderer.prototype = new MASCP.SequenceRenderer();
     };
     var drawAminoAcids = function() {
         var renderer = this;
-        renderer.addTextTrack(this.sequence,this._canvas.set());
+        var aas = renderer.addTextTrack(this.sequence,this._canvas.set());
+        aas.attr({'y' : 12*renderer._RS});
         renderer.select = function() {
             var vals = Array.prototype.slice.call(arguments);
             var from = vals[0];
@@ -940,7 +941,7 @@ MASCP.CondensedSequenceRenderer.prototype.addTextTrack = function(seq,container)
             a_text.style.fontFamily = "'Lucida Console', Monaco, monospace";
             x += 1;
         }
-        amino_acids.attr( { 'y':-1000,'width': RS,'text-anchor':'start','height': RS,'font-size':RS,'fill':'#000000'});
+        amino_acids.attr( { 'width': RS,'text-anchor':'start','height': RS,'font-size':RS,'fill':'#000000'});
     }
     var update_sequence = function() {
         if (seq.length <= 1500) {
@@ -984,6 +985,7 @@ MASCP.CondensedSequenceRenderer.prototype.addTextTrack = function(seq,container)
        }
    },false);
 
+   return amino_acids;
 };
 
 MASCP.CondensedSequenceRenderer.prototype.renderTextTrack = function(lay,in_text) {
