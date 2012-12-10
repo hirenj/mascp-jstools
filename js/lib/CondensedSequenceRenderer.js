ae1b8d58778f09b75bd70d213682a6f59efae881
Hiren Joshi <hirenj@gmail.com>
2012-12-10 14:07:40 +0100

    Fixes for size of callout
    Former-commit-id: ff5f2db89c0472dcc4328f3d556736e66727b00d

--- js/lib/CondensedSequenceRenderer.js
+++ js/lib/CondensedSequenceRenderer.js
@@ -929,7 +929,7 @@ var addCalloutToLayer = function(layerName,element,opts) {
         return;
     }
     var callout = canvas.callout(this._index+0.5,0.01,element,{'width' : (10*opts.width) || 100 ,'height': (opts.height * 10) || 100, 'align' : opts.align });
-    callout.setAttribute('height',this._renderer._RS*10);
+    callout.setHeight(opts.height*this._renderer._RS);
     this._renderer._canvas_callout_padding = Math.max(((10*opts.height) || 100),this._renderer._canvas_callout_padding||0);
     this._renderer._layer_containers[layerName].push(callout);
     callout.clear = function() {
