e1223c4e8184b5817d7c0a589b316013952484f2
Hiren Joshi <hirenj@gmail.com>
2013-01-31 11:13:44 +0100

    Optimisation fix for redrawing axis.
    After playing around in the Speed Tracer, it became evident
    that there were a number of blocking blocks of code that were
    slowing things down. We can make some quick and easy optimisations
    to beat this.
    
    
    Former-commit-id: b4dff48e58927072f99edfb6ceb8786a870d6ca9

--- js/lib/CondensedSequenceRenderer.js
+++ js/lib/CondensedSequenceRenderer.js
@@ -335,9 +335,14 @@ MASCP.CondensedSequenceRenderer.prototype = new MASCP.SequenceRenderer();
         little_labels.hide();
 
         huge_labels.hide();
-
+        var zoom_status = null;
         var zoomchange = function() {
                if (this.zoom > 3.6) {
+                   if (zoom_status == 1) {
+                        return;
+                   }
+                   zoom_status = 1;
+
                    little_ticks.hide();
                    big_ticks.show();
                    big_ticks.attr({'stroke-width' : 0.05*RS+'pt', 'stroke' : '#999999', 'transform' : 'scale(1,0.1) translate(0,4500)' });
@@ -349,6 +354,11 @@ MASCP.CondensedSequenceRenderer.prototype = new MASCP.SequenceRenderer();
                        this._visibleTracers().show();
                    }
                } else if (this.zoom > 1.8) {
+                   if (zoom_status == 2) {
+                        return;
+                   }
+                   zoom_status = 2;
+
                    axis.show();
                    big_ticks.show();
                    axis.attr({'stroke-width':0.5*RS+'pt'});
@@ -364,6 +374,11 @@ MASCP.CondensedSequenceRenderer.prototype = new MASCP.SequenceRenderer();
                        this.tracers.hide();
                    }
                } else if (this.zoom > 0.2) {
+                   if (zoom_status == 3) {
+                        return;
+                   }
+                   zoom_status = 3;
+
                    if (this.tracers) {
                        this.tracers.hide();
                    }
@@ -377,6 +392,10 @@ MASCP.CondensedSequenceRenderer.prototype = new MASCP.SequenceRenderer();
                    little_labels.hide();
                    huge_labels.hide();
                } else {
+                   if (zoom_status == 4) {
+                        return;
+                   }
+                   zoom_status = 4;
                    if (this.tracers) {
                        this.tracers.hide();
                    }
