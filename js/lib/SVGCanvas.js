1244c32f4b3c898ad213898dd8110531e0b4eacc
Hiren Joshi <hirenj@gmail.com>
2013-10-14 15:43:45 +0200

    Rotate elements around 90 degrees
    Former-commit-id: a1c9183ac565dcf7f06c55f7c0513bd3e8c02be2

--- js/lib/SVGCanvas.js
+++ js/lib/SVGCanvas.js
@@ -784,6 +784,7 @@ var SVGCanvas = SVGCanvas || (function() {
             } else if (Array.isArray && Array.isArray(symbol)) {
                 marker.contentElement = this.group();
                 var phase = (2 * Math.PI / symbol.length);
+                phase -= (Math.PI / 2);
                 var needs_stretch = opts.stretch;
                 symbol.forEach(function(symb,i) {
                     var new_el;
@@ -791,6 +792,7 @@ var SVGCanvas = SVGCanvas || (function() {
                     var y_pos = 0.5 + (3 * Math.sin(i*phase));
 
                     var rotate_amount = 360*i/symbol.length;
+                    rotate_amount -= 90;
                     if (needs_stretch) {
                         if (rotate_amount > 90 && rotate_amount < 270 ) {
                             opts.stretch = 'left';
