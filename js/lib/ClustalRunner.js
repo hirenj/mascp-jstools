bc0a3e439bc05ee94b258f5149b6bad9fab80b06
Hiren Joshi <hirenj@gmail.com>
2014-01-15 16:06:43 +0100

    Fixes for some elements not rendering after alignment
    Former-commit-id: 789fdd670a7000c585028ac07d4685abd7550167

--- js/lib/ClustalRunner.js
+++ js/lib/ClustalRunner.js
@@ -297,6 +297,9 @@ MASCP.ClustalRunner.prototype.setupSequenceRenderer = function(renderer) {
                     if ( ! el ) {
                         el = { "_index" : -100, "_renderer" : renderer };
                     }
+                    if ( ! el['_renderer']) {
+                        el['_renderer'] = renderer;
+                    }
 
                     result._index = el._index;
 
