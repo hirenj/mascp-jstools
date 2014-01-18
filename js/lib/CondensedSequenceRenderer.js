e887dbde3a1a82c9f0876ec8b6ed0e0c4f1afa93
Hiren Joshi <hirenj@gmail.com>
2014-01-18 17:34:32 +0100

    Remove jquery for svgready event
    Former-commit-id: 4737c1353265b07c3ed75bbacdd55dfe8853310b

--- js/lib/CondensedSequenceRenderer.js
+++ js/lib/CondensedSequenceRenderer.js
@@ -226,7 +226,7 @@ MASCP.CondensedSequenceRenderer.prototype = new MASCP.SequenceRenderer();
             renderer._object = this;
             renderer._canvas = canv;
             renderer._canvas._canvas_height = 0;
-            jQuery(renderer).trigger('svgready');
+            bean.fire(renderer,'svgready');
         },false);
     
         return canvas;
@@ -574,7 +574,8 @@ MASCP.CondensedSequenceRenderer.prototype = new MASCP.SequenceRenderer();
 
         var RS = this._RS;
 
-        jQuery(this).unbind('svgready').bind('svgready',function(cnv) {
+        bean.remove(this,'svgready');
+        bean.add(this,'svgready',function(cnv) {
             var canv = renderer._canvas;
             canv.RS = RS;
             canv.setAttribute('background', '#000000');
