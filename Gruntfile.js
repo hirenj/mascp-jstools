9ae49e2bb27dad0588ca43002a4da198db036920
Hiren Joshi <hirenj@gmail.com>
2014-07-24 09:27:53 +0200

    Move across to using grunt for building
    Former-commit-id: 2887f70cca8ab9710e29e878ff45fb13da92f6c2

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      build: {
+        files: {
+          'build/<%= pkg.name %>.services.min.js' : ['build/<%= pkg.name %>.services.js'],
+          'build/<%= pkg.name %>.map.min.js' : ['build/<%= pkg.name %>.map.js'],
+          'build/<%= pkg.name %>.renderers.min.js' : ['build/<%= pkg.name %>.renderers.js'],
+          'build/<%= pkg.name %>.min.js' : ['build/<%= pkg.name %>.js']
+         }
+      }
+    },
+    copy: {
+      dist: {
+        expand: true,
+        cwd: 'build/',
+        src: '**',
+        dest: 'dist/js/',
+        flatten: true,
+        filter: 'isFile'
+      }
+    },
+    concat: {
+      basic: {
+        files : {
+          'build/<%= pkg.name %>.services.js' : ['js/lib/MascpService.js','js/lib/*Reader.js','js/lib/*Runner.js','js/lib/MascotToJSON.js'],
+          'build/<%= pkg.name %>.map.js' : [ 'js/lib/gomap.js'],
+          'build/<%= pkg.name %>.renderers.js' : [ 'js/hammer.js','js/jsandbox.js','js/lib/SequenceRenderer.js','js/lib/SVGCanvas.js','js/lib/CondensedSequenceRenderer.js','js/lib/CondensedSequenceRendererNavigation.js','js/lib/TagVisualisation.js' ],
+          'build/<%= pkg.name %>.js' : [ 'js/bean.js','build/<%= pkg.name %>.services.js','build/<%= pkg.name %>.renderers.js','build/<%= pkg.name %>.map.js' ]
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  // Default task(s).
+  grunt.registerTask('default', ['concat','copy','uglify']);
+
+};
