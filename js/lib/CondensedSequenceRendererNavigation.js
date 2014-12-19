63dac357a09437971a57d44107c76a57e9e8823d
Hiren Joshi <hirenj@gmail.com>
2014-12-19 10:31:16 +0100

    Fixing paint rects
    Former-commit-id: 399923126b1c94d6812b667e891596ad933c832f

--- js/lib/CondensedSequenceRendererNavigation.js
+++ js/lib/CondensedSequenceRendererNavigation.js
@@ -466,7 +466,7 @@ MASCP.CondensedSequenceRenderer.Navigation = (function() {
         rect2.removeAttribute('id');
         rect2.removeAttribute('opacity');
         rect2.setAttribute('x','0');
-        rect2.setAttribute('width',""+(parseInt(rect2.getAttribute('width') - 10));
+        rect2.setAttribute('width',""+(parseInt(rect2.getAttribute('width')) - 10));
     
         back_canvas.insertBefore(clipping,back_canvas.firstChild);
         clipping.appendChild(rect2);
