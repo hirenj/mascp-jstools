6064e100006eb45a9f43bd3ff1098d9d2a76e3bf
Hiren Joshi <hirenj@gmail.com>
2014-12-19 10:37:38 +0100

    These paint rects just wont fix themselves
    Former-commit-id: e3312e3af5326e67853e069781386a9c0726d797

--- js/lib/CondensedSequenceRendererNavigation.js
+++ js/lib/CondensedSequenceRendererNavigation.js
@@ -468,6 +468,7 @@ MASCP.CondensedSequenceRenderer.Navigation = (function() {
         rect2.setAttribute('x','0');
         rect2.setAttribute('width',""+(parseInt(rect2.getAttribute('width')) - 10));
         rect2.removeAttribute('style');
+        rect2.setAttribute('height','10000');
     
         back_canvas.insertBefore(clipping,back_canvas.firstChild);
         clipping.appendChild(rect2);
