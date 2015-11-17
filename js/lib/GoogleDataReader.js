bcade1112dd5ac21b811cd2956cc7cfcce040aa0
Hiren Joshi <hirenj@gmail.com>
2015-11-17 08:21:58 +0100

    Fix flow for error when reading a google data file
    Former-commit-id: 8ae08dac7e0852e8722e2809d3bf8453e8cffcee

--- js/lib/GoogleDataReader.js
+++ js/lib/GoogleDataReader.js
@@ -1265,6 +1265,7 @@ MASCP.GoogledataReader.prototype.getSyncableFile = function(file,callback,mime) 
     get_file(file,mime,function(err,filedata,file_id) {
         if (err) {
             callback.call(null,err);
+            return;
         }
         file_block.getData = function() {
             return filedata;
