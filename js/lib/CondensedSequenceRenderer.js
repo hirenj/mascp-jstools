ecdfd4b5a6a4c3398e9fdf7b753ef10221204f42
Hiren Joshi <hirenj@gmail.com>
2016-02-22 22:31:27 +0100

    Fix for window object identifying as an object now
    Former-commit-id: fba5f4ed030e74c0cceb028d6627ea31d908d87c

--- js/lib/CondensedSequenceRenderer.js
+++ js/lib/CondensedSequenceRenderer.js
@@ -628,7 +628,7 @@ MASCP.CondensedSequenceRenderer.prototype = new MASCP.SequenceRenderer();
     clazz.prototype.win = function() {
         if (this._container && this._container.ownerDocument && this._container.ownerDocument.defaultView) {
             var return_val = this._container.ownerDocument.defaultView;
-            if (typeof return_val === 'object') {
+            if (typeof return_val === 'object' && return_val.constructor !== Window ) {
                 return_val = return_val[Object.keys(return_val)[0]];
             }
             return return_val;
