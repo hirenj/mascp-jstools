c1ed116b8d6983e9afb16334ed85eb81fecc8a34
Hiren Joshi <hirenj@gmail.com>
2017-01-24 23:41:45 +0100

    Add fix for tracks magically shrinking
    Former-commit-id: 9f083abdddab886dcce9ded05cd9d22e57937a25

--- js/lib/CondensedSequenceRenderer.js
+++ js/lib/CondensedSequenceRenderer.js
@@ -1913,8 +1913,11 @@ MASCP.CondensedSequenceRenderer.prototype.renderObjects = function(track,objects
     var results = [];
     objects.forEach(function(object) {
         var potential_height = object.options ? (object.options.height || renderer._layer_containers[track].track_height) + (object.options.offset + object.options.height || 0) : 0;
-        if (object.options && potential_height > renderer._layer_containers[track].track_height) {
-            renderer._layer_containers[track].fixed_track_height = renderer._layer_containers[track].track_height + object.options.offset + (object.options.height || renderer._layer_containers[track].track_height);
+        if (object.options && (potential_height > renderer._layer_containers[track].track_height) ) {
+            var new_height = renderer._layer_containers[track].track_height + object.options.offset + (object.options.height || renderer._layer_containers[track].track_height);
+            if ((renderer._layer_containers[track].fixed_track_height || 0) < new_height) {
+                renderer._layer_containers[track].fixed_track_height = new_height;
+            }
         }
 
         var click_reveal;
